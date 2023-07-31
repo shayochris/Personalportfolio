7756bc0d14c6e33cf43f4eb91bd5be6c8e8a396c
chris <chrisshayo4@gmail.com>
2023-07-31 14:13:33 -0700

    final touches to version 1 phase 1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 import { ThemeContext } from "../contexts/ThemeContext"
 import Navbar from "./Navbar";
 import { AiFillGithub,AiFillLinkedin,AiFillFacebook,AiFillInstagram } from "react-icons/ai";
+import chris from "../assets/images/chris.jpg"
 export default function Home() {
   const {theme,ui}=useContext(ThemeContext);
   return (
@@ -11,7 +12,9 @@ export default function Home() {
           <Navbar/>
           <div className="flex sm:flex-row flex-col relative top-20 mb-4">
             <div className="w-full sm:h-[300px] flex justify-center items-center">
-              <div className={`md:h-[200px] h-[150px] md:w-[200px] w-[150px] rounded-full ${ui}`}></div>
+              <div className={`md:h-[200px] h-[150px] md:w-[200px] w-[150px] rounded-full overflow-hidden ${ui}`}>
+                <img src={chris} alt="" className="w-full h-full object-cover bg-center" />
+              </div>
             </div>
 
             <div className="w-full sm:h-[300px] flex flex-col justify-center sm:items-start items-center mt-6 sm:mt-0">
